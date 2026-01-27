1ac74b86964cbd92d8ea3e227f22bff6313d23b1
guitch95 <guitch95@gmail.com>
2026-01-27 17:15:10 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Guillaume Richard on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
