fae12525941989e781f9bb2ce848b8336292e682
guitch95 <guitch95@gmail.com>
2026-01-28 15:11:20 +0100

    Final layout.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,58 +9,73 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
 
+    // static can be read whenever we want.
+    // It makes the computed variable belong to the struct itself
+    // rather than one instance.
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("When do you want to wake up ?")
-                    .font(.headline)
-                DatePicker(
-                    "Please enter a time",
-                    selection: $wakeUp,
-                    displayedComponents: .hourAndMinute
-                )
-                .labelsHidden()
-                // hidden from the interface but still there for voiceover
+        ZStack {
+            Color.red
+                .ignoresSafeArea()
 
-                Text("Desire amount of sleep")
-                    .font(.headline)
+            NavigationStack {
+                VStack {
+                    Form {
+                        Section("When do you want to wake up ?") {
+                            DatePicker(
+                                "Please enter a time",
+                                selection: $wakeUp,
+                                displayedComponents: .hourAndMinute
+                            )
+                            //                    .labelsHidden()
+                        }
+                        // hidden from the interface but still there for voiceover
 
-                Stepper(
-                    "\(sleepAmount.formatted())hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25
-                )
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(
-                    "\(coffeeAmount) cup(s)",
-                    value: $coffeeAmount,
-                    in: 1...20
-                )
+                        Section("Desire amount of sleep") {
+                            Stepper(
+                                "\(sleepAmount.formatted()) hours",
+                                value: $sleepAmount,
+                                in: 4...12,
+                                step: 0.25
+                            )
+                        }
+
+                        Section("Daily coffee intake") {
+                            Stepper(
+                                // Handles the pluralization of the word automatically
+                                "^[\(coffeeAmount) cup](inflect:true)",
+                                value: $coffeeAmount,
+                                in: 1...20
+                            )
+                        }
+                    }
+                    .navigationTitle("BetterRest")
+                    .toolbar {
+                        Button("Calculate", action: calculateBedTime)
+                    }
+                    .alert(alertTitle, isPresented: $showingAlert) {
+                        Button("Ok") {}
+
+                    } message: {
+                        Text(alertMessage)
+                    }
+
+                }
 
             }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate") {
-                    calculateBedTime()
-                }
-                .alert(alertTitle, isPresented: $showingAlert) {
-                    Button("Ok") {}
-
-                } message: {
-                    Text(alertMessage)
-                }
-            }
-
         }
     }
     func calculateBedTime() {
