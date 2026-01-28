21a5cdd313c29178339551a1c099b9e0d5a5d109
guitch95 <guitch95@gmail.com>
2026-01-28 14:39:19 +0100

    Updated ContentView for the Layout.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,18 @@
 //  Created by Guillaume Richard on 27/01/2026.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
     var body: some View {
         NavigationStack {
             VStack {
@@ -47,12 +53,59 @@ struct ContentView: View {
                 Button("Calculate") {
                     calculateBedTime()
                 }
+                .alert(alertTitle, isPresented: $showingAlert) {
+                    Button("Ok") {}
+
+                } message: {
+                    Text(alertMessage)
+                }
             }
+
         }
     }
-    func calculateBedTime() {}
+    func calculateBedTime() {
+        // CoreML can throw error, this is why we have a catch in order to "catch" the error.
+        do {
+            // Config de notre Model de ML
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents(
+                [
+                    .hour,
+                    .minute,
+                ],
+                from: wakeUp
+            )
+            // Optionals so if it can't be read it gonna apply 0
+            // To get seconds from hour
+            let hour = (components.hour ?? 0) * 60 * 60
+            // To get seconds from minute
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(
+                wake: Double(hour + minute),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount)
+            )
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+        } catch {
+            // Catch en eventual error
+            alertTitle = "Error"
+            alertMessage =
+                "Sorry, there was a problem calculating your bedtime. Please try again."
+        }
+        showingAlert.toggle()
+    }
+
 }
 
 #Preview {
     ContentView()
 }
+
+// INTEGRATION BACKGROUNG LINEARGRADIENT COULEUR COFFEE BROWN/RED VIA STACK
